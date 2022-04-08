bd0c76f8a692939aff48dcb127a17a71b02f17d2
Himanshu Pandey <himanshu.pandey01@sap.com>
2022-04-08 13:32:00 +0530

    deadlock resolved

--- web.js
+++ web.js
@@ -11,16 +11,18 @@ app.get('/', function (req, res) {
 var gotHitCount = 0;
 var hitCount = 0;
 var noHitCount = 0;
-var prevScore = 0;
+var prevScore = -100;
+var lowCount = 0
+var prevLocation = [];
 
 app.post('/', function (req, res) {
-  console.log(JSON.stringify(req.body));
+  //console.log(JSON.stringify(req.body));
 
   let arena = req.body.arena;
 
   let me = req.body._links.self.href;
 
-  console.log(me);
+  //console.log(me);
 
   let myData = arena.state[me];
 
@@ -33,40 +35,72 @@ app.post('/', function (req, res) {
 
 let decideMove = (me, myData, arena) => {
 
-  let players = Object.keys(arena.state);
-  if(myData.wasHit){
-    gotHitCount ++;
+  if (prevScore > myData.score) {
+    lowCount++;
+  } else {
+    lowCount = 0;
   }
+  if (lowCount > 4 && prevLocation[0] == myData.x && prevLocation[1] == myData.y) {
+    lowCount = 3;
+    prevScore = myData.score;
+    prevLocation = [myData.x, myData.y]
+    return 'R';
+  }
+  if (lowCount >= 4) {
+    prevScore = myData.score;
+    prevLocation = [myData.x, myData.y]
+    return 'F';
+  }
+  if (lowCount >= 3) {
+    prevScore = myData.score;
+    prevLocation = [myData.x, myData.y]
+    return changeDirection(myData, arena);
+  }
+  let players = Object.keys(arena.state);
+
+  if (myData.wasHit) {
+    gotHitCount++;
+  }
+
   let hitRange = getHitRange(myData, arena)
 
-  if(hitRange.length == 0){
+  if (hitRange.length == 0) {
+    prevScore = myData.score;
+    prevLocation = [myData.x, myData.y]
     return changeDirection(myData, arena)
   }
-  
-  for( let i = 0; i< players.length; i++){
 
-    if(players[i] === me ){
+  for (let i = 0; i < players.length; i++) {
+
+    if (players[i] === me) {
       continue;
     }
 
-    if(players[i] == "https://cloud-run-hackathon-nodejs-wqy7qm7s3a-as.a.run.app"){
-      console.log(i);
-    }
-
-    if(inRange(hitRange, arena.state[players[i]])){
+    if (inRange(hitRange, arena.state[players[i]])) {
       hitCount++;
       noHitCount = 0;
+      prevScore = myData.score;
+      prevLocation = [myData.x, myData.y]
       return 'T';
     }
   }
   noHitCount++;
-  if(noHitCount > 8){
+
+  if (noHitCount > 5) {
+    hitCount = 0;
     noHitCount = 0;
+    prevScore = myData.score;
+    prevLocation = [myData.x, myData.y]
     return 'F';
   }
-  if(noHitCount > 5){
+  if (noHitCount > 3) {
+    hitCount = 0;
+    prevScore = myData.score;
+    prevLocation = [myData.x, myData.y]
     return changeDirection(myData, arena);
   }
+  prevScore = myData.score;
+  prevLocation = [myData.x, myData.y]
   return changeDirection(myData, arena);
 }
 
@@ -75,38 +109,38 @@ let getHitRange = (myData, arena) => {
 
   let range = 3;
   let coordinates = []
-  let x =  myData.x;
-  let y =  myData.y;
+  let x = myData.x;
+  let y = myData.y;
 
-  if(myData.direction == 'E'){
-    
+  if (myData.direction == 'E') {
+
     range = Math.min(3, arena.dims[0] - x - 1);
-    while(range > 0){
+    while (range > 0) {
       ++x;
       coordinates.push([x, y]);
       --range;
     }
-  } else if(myData.direction == 'N'){
-    
+  } else if (myData.direction == 'N') {
+
     range = Math.min(3, y);
-    while(range > 0){
+    while (range > 0) {
       --y;
       coordinates.push([x, y]);
       --range;
     }
 
-  } else if(myData.direction == 'W'){
-    
+  } else if (myData.direction == 'W') {
+
     range = Math.min(3, x);
-    while(range > 0){
+    while (range > 0) {
       --x;
       coordinates.push([x, y]);
       --range;
     }
-  } else{
-    
+  } else {
+
     range = Math.min(3, arena.dims[1] - y - 1);
-    while(range > 0){
+    while (range > 0) {
       ++y;
       coordinates.push([x, y]);
       --range;
@@ -118,24 +152,24 @@ let getHitRange = (myData, arena) => {
 }
 
 let changeDirection = (myData, arena) => {
-  
-  if(myData.direction == 'E'){
+
+  if (myData.direction == 'E') {
     right = arena.dims[1] - myData.y - 1;
     left = Math.min(3, myData.y);
-  } else if(myData.direction == 'N'){
+  } else if (myData.direction == 'N') {
     right = arena.dims[0] - myData.x - 1;
     left = Math.min(3, myData.x);
-  } else if(myData.direction == 'W'){
-    left = arena.dims[1] - myData.y - 1 ;
+  } else if (myData.direction == 'W') {
+    left = arena.dims[1] - myData.y - 1;
     right = Math.min(3, myData.y);
-  } else{
-    left = arena.dims[0] - myData.x - 1 ;
+  } else {
+    left = arena.dims[0] - myData.x - 1;
     right = Math.min(3, myData.x);
   }
 
-  if(right >= left){
+  if (right >= left) {
     return 'R';
-  } else{
+  } else {
     return 'L';
   }
 }
@@ -144,8 +178,8 @@ let inRange = (hitRange, player) => {
 
   let j = 0;
   for (; j < hitRange.length; j++) {
-    if(hitRange[j][0] == player.x && hitRange[j][1] == player.y){
-        return true;
+    if (hitRange[j][0] == player.x && hitRange[j][1] == player.y) {
+      return true;
     }
   }
   return false;
